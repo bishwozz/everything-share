ac80e1bfb2d1beef2d2c789d5440580d43f4d59e
bishwozz <bishwo123sapkota@gmail.com>
2024-09-12 15:13:59 +0545

    base init

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController;
-
-Route::get('/', function () {
-    return view('react');
-});
-Route::get('/api/data', [ApiController::class, 'getData']);
